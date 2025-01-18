7bff3d6240fb2bfbbd043bf35f00d4bb11009e99
bipedalmonkey <aussienonsensegm@gmail.com>
2025-01-19 00:49:41 +1030

    fix australian spelling in neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,9 +1,26 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
+local opts = {
+  filesystem = {
+    bind_to_cwd = true,
+    filtered_items = {
+      visible = true,
+    },
+
+    hijack_netrw_behavior = 'open_current',
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
+      },
+    },
+  },
+}
+
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
+  lazy = true,
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
@@ -17,6 +34,7 @@ return {
   init = function()
     -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
     -- because `cwd` is not set up properly.
+    -- this stops netrw flashing on startup
     vim.api.nvim_create_autocmd('BufEnter', {
       group = vim.api.nvim_create_augroup('Neotree_start_directory', { clear = true }),
       desc = 'Start Neo-tree with directory',
@@ -27,29 +45,12 @@ return {
         else
           local stats = vim.uv.fs_stat(vim.fn.argv(0))
           if stats and stats.type == 'directory' then
-            require('neo-tree').setup {
-              filesystem = {
-                hijack_netrw_behavior = 'open_current',
-                window = {
-                  mappings = {
-                    ['\\'] = 'close_window',
-                  },
-                },
-              },
-            }
+            require('neo-tree').setup(opts)
           end
         end
       end,
     })
   end,
-  opts = {
-    filesystem = {
-      hijack_netrw_behaviour = 'open_current',
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+
+  opts = opts,
 }
